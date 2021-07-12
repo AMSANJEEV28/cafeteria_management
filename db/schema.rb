5019a01b9fe2168db06c3e0df07a0f1a0476551e
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-12 15:59:17 +0530

    additional data to users table added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_101621) do
+ActiveRecord::Schema.define(version: 2021_07_12_102525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2021_07_12_101621) do
     t.string "last_name"
     t.string "first_name"
     t.string "password_digest"
+    t.bigint "phone_number"
+    t.string "role"
   end
 
 end
