27b3a90454dfa8421005a2bc991f71ab8d93634f
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-12 17:46:58 +0530

    required controllers, models, tables added

--- db/migrate/20210712103133_create_deliver_order.rb
+++
@@ -1,8 +0,0 @@
-class CreateDeliverOrder < ActiveRecord::Migration[6.1]
-  def change
-    create_table :deliver_orders do |t|
-      add_column :orders, :delivered_at, :datetime
-      t.timestamps
-    end
-  end
-end
