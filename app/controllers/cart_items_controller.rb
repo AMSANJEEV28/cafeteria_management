56df1532a82d454c0686c8c26b068884abcee182
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-13 02:02:23 +0530

    can add items now

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,46 @@
 class CartItemsController < ApplicationController
   def increment
-    cart_item_id = params[:id]
-    order_item = CartItem.find_by(menu_item_id: params[:id])
-    order_item.quantity = order_item.quantity + 1
-    order_item.save
-    flash[:notice] = "+1 #{order_item.menu_item_name}"
-    redirect_to menus_path
+    menu_item_id = params[:id]
+    cart_item = CartItem.find_by(menu_item_id: params[:id])
+    cart_item.quantity = cart_item.quantity + 1
+    cart_item.price = cart_item.quantity * cart_item.menu_item_price
+    cart_item.save
+    redirect_to "/menu_categories"
+  end
+
+  def decrement
+    menu_item_id = params[:id]
+    cart_item = CartItem.find_by(menu_item_id: params[:id])
+    if cart_item.quantity > 1
+      cart_item.quantity = cart_item.quantity - 1
+      cart_item.price = cart_item.quantity * cart_item.menu_item_price
+      cart_item.save
+      redirect_to "/menu_categories"
+    else
+      destroy
+    end
+  end
+
+  def create
+    menu_item = MenuItem.find(params[:id])
+    cart_id = current_user.cart.id
+    cart_item = CartItem.new(
+      cart_id: cart_id,
+      menu_item_id: menu_item.id,
+      menu_item_name: menu_item.name,
+      menu_item_price: menu_item.price,
+      quantity: 1,
+      price: menu_item.price,
+    )
+    if cart_item.save!
+      redirect_to "/menu_categories"
+    end
+  end
+
+  def destroy
+    menu_item_id = params[:id]
+    cart_item = CartItem.find_by(menu_item_id: params[:id])
+    cart_item.destroy
+    redirect_to "/menu_categories"
   end
 end
