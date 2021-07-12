caf78a424dbdeea2ecf6713203c2a49392e890a4
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-13 01:59:07 +0530

    can add items now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,46 @@
-class ApplicationController < ActionController::Base
-    before_action :ensure_user_logged_in
+class CartItemsController < ApplicationController
+  def increment
+    menu_item_id = params[:id]
+    cart_item = CartItem.find_by(menu_item_id: params[:id])
+    cart_item.quantity = cart_item.quantity + 1
+    cart_item.price = cart_item.quantity * cart_item.menu_item_price
+    cart_item.save
+    redirect_to "/menu_categories"
+  end
 
-  def ensure_user_logged_in
-    unless current_user
-      redirect_to "/"
+  def decrement
+    menu_item_id = params[:id]
+    cart_item = CartItem.find_by(menu_item_id: params[:id])
+    if cart_item.quantity > 1
+      cart_item.quantity = cart_item.quantity - 1
+      cart_item.price = cart_item.quantity * cart_item.menu_item_price
+      cart_item.save
+      redirect_to "/menu_categories"
+    else
+      destroy
     end
   end
 
-  def current_user
-    return @current_user if @current_user
-    current_user_id = session[:current_user_id]
-    if current_user_id
-      @current_user = User.find(current_user_id)
-    else
-      nil
+  def create
+    menu_item = MenuItem.find(params[:id])
+    cart_id = current_user.cart.id
+    cart_item = CartItem.new(
+      cart_id: cart_id,
+      menu_item_id: menu_item.id,
+      menu_item_name: menu_item.name,
+      menu_item_price: menu_item.price,
+      quantity: 1,
+      price: menu_item.price,
+    )
+    if cart_item.save!
+      redirect_to "/menu_categories"
     end
+  end
+
+  def destroy
+    menu_item_id = params[:id]
+    cart_item = CartItem.find_by(menu_item_id: params[:id])
+    cart_item.destroy
+    redirect_to "/menu_categories"
   end
 end
