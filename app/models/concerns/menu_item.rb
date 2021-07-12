999cbaa1d8979248a29ea0223a858a6dd06074a8
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-12 17:52:54 +0530

    menu item table

--- app/models/concerns/menu_item.rb
+++ app/models/concerns/menu_item.rb
@@ -1,2 +1,15 @@
 class MenuItem < ApplicationRecord
-end
+    belongs_to :menu_category
+  
+    def self.get_items(id)
+      where("menu_category_id = ?", id)
+    end
+  
+    def self.get_item_names(id)
+      where("menu_category_id = ?", id).pluck(:name, :id)
+    end
+  
+    def self.get_item_values(menu_id)
+      where("id = ?", menu_id).pluck(:name, :description, :price)
+    end
+  end
