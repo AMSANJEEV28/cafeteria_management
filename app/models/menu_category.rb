7a03449b0d6fcd66b174134e588ce3bc69a7ee46
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-12 17:53:38 +0530

    menu category table

--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -1,2 +1,26 @@
 class MenuCategory < ApplicationRecord
-end
+    has_many :menu_items
+  
+    def self.get_menu_names
+      MenuCategory.all.map { |menu| menu }
+    end
+  
+    def self.get_name
+      MenuCategory.pluck(:name, :id)
+    end
+  
+    def isActive?
+      active
+    end
+  
+    def makeActive
+      MenuCategory.all.map { |menu|
+        menu.active = false
+        menu.save
+      }
+      menu = MenuCategory.find(:params[id])
+      menu.active = true
+      menu.save
+      MenuCategory.all.map { |menu| puts "#{menu.name} #{menu.active}" }
+    end
+  end
