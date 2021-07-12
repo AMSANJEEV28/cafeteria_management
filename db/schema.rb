c2bdecbd9a085918c248690c70ebb7569e07adbd
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-12 15:49:59 +0530

    order item table + models added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_101213) do
+ActiveRecord::Schema.define(version: 2021_07_12_101621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2021_07_12_101213) do
     t.string "name"
     t.text "about_item"
     t.money "cost", scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "menu_item_id"
+    t.string "menu_item_name"
+    t.money "menu_item_cost", scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
